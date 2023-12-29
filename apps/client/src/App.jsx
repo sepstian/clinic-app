8b8b9db7993755391ed676b98115e33f8be9ce52
Khansa Kamila <142070814+Emma0123@users.noreply.github.com>
2023-12-29 20:11:33 +0700

    feat: queuepage, detailpage (#4)

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,11 +1,14 @@
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import "./App.css";
+import DetailPage from "./pages/DetailPage";
+import QueuePage from "./pages/quequePage";
 import LoginPage from './pages/Login/Login';
-
 
 function App() {
   return (
     <>
+      <QueuePage/>
+      {/* <DetailPage/> */}
       <Routes>
         <Route path='/' element={<LoginPage/>}/>
       </Routes>
---
+++ apps/client/src/components/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SideNav() {
+    return (
+        <>
+            <div className='w-[300px] bg-[#8F8E8E] h-full'></div>
+        </>
+    )
+}
+
+export default SideNav;
